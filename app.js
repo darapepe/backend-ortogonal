94e316e0cfda7f9529dd3fa769233ec9ddc1bf97
Darwin Perez Perez <perez.darwin@gmail.com>
2022-07-10 18:30:40 -0500

    make it better

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http')
+
+let hostname = 'localhost'
+let port = 3000
+
+function funcion(req, res) {
+    const vara1 = req.headers.a1
+    const vara2 = req.headers.a2
+    const varb1 = req.headers.b1
+    const varb2 = req.headers.b2
+    var result = (vara1 * varb1 + vara2 * varb2)
+    
+    var calculo = ""
+        if (result === 0) {
+            calculo = "Es Ortogonal"
+        } else {
+            calculo = "No Es Ortogonal"
+        }
+        //const calculo = result = 0 ? "Es Ortogonal" : "No es Ortogonal";
+        res.statusCode = 200
+        res.setHeader('Content-type', 'application/json')
+        res.end(JSON.stringify({ resultado: `${calculo}`, calculo: `${result}` }))
+}
+
+let server = http.createServer(funcion)
+
+server.listen(port, hostname, () => {
+    console.log('Servidor ejecutandose')
+})
