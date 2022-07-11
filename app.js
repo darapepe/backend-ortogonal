18fecc5da2d9227ad1b428a8c28f195a1384903f
Darwin Perez Perez <perez.darwin@gmail.com>
2022-07-10 21:08:26 -0500

    make it better

--- app.js
+++ app.js
@@ -3,25 +3,28 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-http.createServer( (req, res) => {
+http.createServer((req, res) => {
     const vara1 = req.headers.a1
     const vara2 = req.headers.a2
     const varb1 = req.headers.b1
     const varb2 = req.headers.b2
     var result = (vara1 * varb1 + vara2 * varb2)
-    
+
     var calculo = ""
-        if (result === 0) {
-            calculo = "Es Ortogonal"
-        } else {
-            calculo = "No Es Ortogonal"
-        }
-        //const calculo = result = 0 ? "Es Ortogonal" : "No es Ortogonal";
-        res.statusCode = 200
-        res.setHeader('Content-type', 'application/json')
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.end(JSON.stringify({ resultado: `${calculo}`, calculo: `${result}` }))
+    if (result === 0) {
+        calculo = "Es Ortogonal"
+    } else {
+        calculo = "No Es Ortogonal"
+    }
+    //const calculo = result = 0 ? "Es Ortogonal" : "No es Ortogonal";
+    res.statusCode = 200
+    res.setHeader('Content-type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.header("Access-Control-Allow-Credentials", true)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    res.end(JSON.stringify({ resultado: `${calculo}`, calculo: `${result}` }))
 }).listen(PORT, err => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("%c Server running", "color: green");
 })
