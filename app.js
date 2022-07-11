b9119447519a5ad4bcb2ffb0998eeffc94a661ba
Darwin Perez Perez <perez.darwin@gmail.com>
2022-07-10 21:20:48 -0500

    make it better

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-type', 'application/json')
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader("Access-Control-Allow-Credentials", true)
+    res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     //res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
-    res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
     res.end(JSON.stringify({ resultado: `${calculo}`, calculo: `${result}` }))
 }).listen(PORT, err => {
     if (err) throw err;
